e027d3475c1299bb5926874dee8a42fe5cd76258
Justin Combs <skrabio@gmail.com>
2015-03-28 01:58:32 -0400

    new sidebar and material design

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,3 +4,19 @@ $('body').scrollspy({
     target: '.bs-docs-sidebar',
     offset: 40
 });
+
+var today = new Date();
+var hour = today.getHours();
+var greeting;
+
+if (hour > 18) {
+    greeting = 'Good Evening.' ;
+} else if (hour > 12) {
+    greeting = 'Good Afternoon.' ;
+} else if (hour > 0) {
+    greeting = 'Good Morning.' ;
+} else {
+    greeting = 'Welcome.';
+}
+
+document.getElementById('datejs').innerHTML = greeting;
